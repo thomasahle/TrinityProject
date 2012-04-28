ab339092d69afeed4c2fe18aa7ab122f017a8b68
Thomas Dybdahl Ahle <thomas@ahle.dk>
2012-04-28 21:06:10 +0100

    Removed built files

--- core/target/classes/com/github/thomasahle/trainbox/trainbox/model/Component.java
+++
@@ -1,5 +0,0 @@
-package com.github.thomasahle.trainbox.trainbox.model;
-
-public interface Component {
-	public Train pull();
-}
--- core/target/classes/com/github/thomasahle/trainbox/trainbox/model/Composite.java
+++
@@ -1,7 +0,0 @@
-package com.github.thomasahle.trainbox.trainbox.model;
-
-import java.util.List;
-
-public interface Composite extends Component {
-	public List<Component> getChildren();
-}
