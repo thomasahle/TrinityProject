864348370b8c459cc4a1840c643f196b3a6d5203
Thomas Dybdahl Ahle <thomas@ahle.dk>
2012-05-01 15:54:06 +0100

    Added play button (not connected to track yet)

--- core/src/main/java/com/github/thomasahle/trainbox/trainbox/scenes/LevelScene.java
+++ core/src/main/java/com/github/thomasahle/trainbox/trainbox/scenes/LevelScene.java
@@ -2,6 +2,7 @@ package com.github.thomasahle.trainbox.trainbox.scenes;
 
 import static playn.core.PlayN.graphics;
 import static playn.core.PlayN.pointer;
+import static playn.core.PlayN.log;
 import playn.core.CanvasImage;
 import playn.core.GroupLayer;
 import playn.core.Layer;
@@ -10,11 +11,9 @@ import playn.core.Pointer.Listener;
 import pythagoras.f.Point;
 
 import com.github.thomasahle.trainbox.trainbox.uimodel.TrainsChangedListener;
-import com.github.thomasahle.trainbox.trainbox.uimodel.UIComponent;
 import com.github.thomasahle.trainbox.trainbox.uimodel.UIComposite;
 import com.github.thomasahle.trainbox.trainbox.uimodel.UIDupComponent;
 import com.github.thomasahle.trainbox.trainbox.uimodel.UIHorizontalComponent;
-import com.github.thomasahle.trainbox.trainbox.uimodel.UIIdentityComponent;
 import com.github.thomasahle.trainbox.trainbox.uimodel.UITrain;
 
 
@@ -33,6 +32,8 @@ public class LevelScene implements Scene, Listener {
 	private UIComposite mTrack;
 	private GroupLayer mTrainLayer;
 	
+	private Layer mPlayButton;
+
 	public LevelScene() {
 		// A background image. This should be really nice.
 		CanvasImage bgImage = graphics().createImage(WIDTH, HEIGHT);
@@ -63,7 +64,30 @@ public class LevelScene implements Scene, Listener {
 			}
 		});
 		
+		// Connect the play button to the track
+		mPlayButton = initPlayButton();
+		mPlayButton.addListener(new Listener(){
+			public void onPointerStart(Event event) {
+				log().debug("Start clicked");
+				// TODO: track.togglePause()
+			}
+			public void onPointerEnd(Event event) {}
+			public void onPointerDrag(Event event) {}
+		});
+		
 		mTrack = track;
+	}
+	
+	private Layer initPlayButton() {
+		CanvasImage img = graphics().createImage(100, 100);
+		img.canvas().setFillColor(0xffffffff).fillRect(0, 0, 100, 100);
+		playn.core.Path triangle = img.canvas().createPath();
+		triangle.moveTo(0, 0);
+		triangle.lineTo(87, 50);
+		triangle.lineTo(0,100);
+		triangle.lineTo(0,0);
+		img.canvas().setFillColor(0xff339900).fillPath(triangle);
+		return graphics().createImageLayer(img);
 	}
 	
 	@Override
@@ -82,6 +106,9 @@ public class LevelScene implements Scene, Listener {
 		graphics().rootLayer().add(mTrack.getFrontLayer());
 		
 		pointer().setListener(this);
+		
+		graphics().rootLayer().add(mPlayButton);
+		mPlayButton.setTranslation(graphics().screenWidth()-100, graphics().screenHeight()-100);
 	}
 
 	@Override
@@ -90,13 +117,19 @@ public class LevelScene implements Scene, Listener {
 		graphics().rootLayer().remove(mTrack.getBackLayer());
 		graphics().rootLayer().remove(mTrainLayer);
 		graphics().rootLayer().remove(mTrack.getFrontLayer());
+		
+		pointer().setListener(null);
+		
+		graphics().rootLayer().remove(mPlayButton);
 	}
 
 	@Override
 	public void onPointerStart(Event event) {
 		// FIXME: Is this the right way to get the point, or do we need something localX?
+		// Perhaps we should use track.backLayer().setListener or something?
 		Point p = new Point(event.x(), event.y());
-		mTrack.insertChildAt(new UIDupComponent(80), p);
+		if (event.y() < mTrack.getSize().height && event.x() < mTrack.getSize().width)
+			mTrack.insertChildAt(new UIDupComponent(80), p);
 	}
 	@Override
 	public void onPointerEnd(Event event) {}
