440277fdd02a5bdb0cbccd19a3e339d58527ef39
Matt Pellegrini <mattpellegrini@live.co.uk>
2012-05-03 16:40:50 +0100

    Unfinished and Untested attempt at the end component.

--- core/src/main/java/com/github/thomasahle/trainbox/trainbox/uimodel/UIEndComponent.java
+++ core/src/main/java/com/github/thomasahle/trainbox/trainbox/uimodel/UIEndComponent.java
@@ -10,36 +10,44 @@ import java.util.Deque;
 import java.util.Iterator;
 import java.util.List;
 
+import playn.core.Canvas;
 import playn.core.CanvasImage;
 import playn.core.Layer;
 import pythagoras.f.Dimension;
 import pythagoras.f.Point;
 
-public class UIDupComponent extends AbstractComponent implements UIComponent, TrainTaker {
+//TODO add this to the end of the level for testing.
+
+public class UIEndComponent extends AbstractComponent implements UIComponent, TrainTaker {
 
 	private final static int HEIGHT = 100;
 	private int mWidth;
 	
-	private Layer mBackLayer, mFrontLayer;
+	private Layer mBackLayer, mFrontLayer, mCargoDisplayLayer;
+	private Canvas textCanvas;
 	private Deque<UITrain> mIncomming = new ArrayDeque<UITrain>();
 	private Deque<UITrain> mOutgoing = new ArrayDeque<UITrain>();
-	private Deque<UITrain> mSent = new ArrayDeque<UITrain>();
-	private TrainTaker mTrainTaker;
+	private List<UICarriage> finishedCarriages = new ArrayList<UICarriage>();
+	private String finishedCargoString = "";
 	
-	@Override
-	public void setTrainTaker(TrainTaker listener) {
-		mTrainTaker = listener;
-	}
-	
-	public UIDupComponent(int width) {
+	public UIEndComponent(int width) {
 		mWidth = width;
 		
 		mBackLayer = graphics().createGroupLayer();
 		
 		CanvasImage image = graphics().createImage(width, HEIGHT);
-		image.canvas().setFillColor(0xffaa00aa);
+		image.canvas().setFillColor(0x0000ffaa);
 		image.canvas().fillCircle(width/2.f, HEIGHT/2.f, width/2.f);
 		mFrontLayer = graphics().createImageLayer(image);
+		
+		//TODO find a way to display this layer.
+		CanvasImage ti = graphics().createImage(width,HEIGHT);
+		textCanvas = ti.canvas();
+		mCargoDisplayLayer = graphics().createImageLayer(ti);
+		//Position this layer over the component.
+		mCargoDisplayLayer.setTranslation(this.getPosition().x, this.getPosition().y);
+		
+		
 	}
 
 	@Override
@@ -63,10 +71,6 @@ public class UIDupComponent extends AbstractComponent implements UIComponent, Tr
 
 	@Override
 	public void update(float delta) {
-		// TODO: This works ok now, but we still need some more animation graphics,
-		//		 As a minimum we should indicate if the component is loaded or not.
-		
-		// 		 Perhaps we want to blockLeft if there is already a train here being duplicated? 
 
 		float compLeft = getDeepPosition().x;
 		float compRight = compLeft + getSize().width;
@@ -84,10 +88,6 @@ public class UIDupComponent extends AbstractComponent implements UIComponent, Tr
 				
 				mOutgoing.add(train);
 				train.getLayer().setVisible(false);
-				UITrain clone = new UITrain(train);
-				mOutgoing.add(clone);
-				fireTrainCreated(clone);
-				clone.getLayer().setVisible(false);
 				
 				log().debug("Train captured, moved to middle queue. Length is: "+mOutgoing.size());
 			}
@@ -101,37 +101,38 @@ public class UIDupComponent extends AbstractComponent implements UIComponent, Tr
 		for (Iterator<UITrain> it = mOutgoing.iterator(); it.hasNext(); ) {
 			UITrain train = it.next();
 			
-			if (compRight < mTrainTaker.leftBlock()) {
-				log().debug("Sending a cloned element to "+mTrainTaker);
-				
-				it.remove();
-				train.setPosition(new Point(compRight-train.getSize().width, train.getPosition().y));
-				mTrainTaker.takeTrain(train);
-				train.getLayer().setVisible(true);
-				
-				mSent.add(train);
-				
-				log().debug("Train cloned. Sent to output queue.");
-			}
-		}
-		// As they leave us, dragged by the next component, uncrop them gradually
-		for (Iterator<UITrain> it = mSent.iterator(); it.hasNext(); ) {
-			UITrain train = it.next();
-			float trainLeft = train.getPosition().x;
-			float trainRight = trainLeft + train.getSize().width;
+			/* get the carriages from the train and iterate over this list removing the cargo.
+			 * The idea is that the cargo (numbers at this point) from the carriages will be 
+			 * displayed on the end component or below it.
+			 * 
+			 * Hopefully we will eventually have images representing each type of cargo that we 
+			 * can display on the trains and at this point.
+			 * 
+			 */
 			
-			train.setCropRight(trainRight-compLeft);
-			
-			if (trainLeft >= compRight) {
-				it.remove();
+			List<UICarriage> carriages = train.getCarriages();
+			for(UICarriage c : carriages){
+				finishedCarriages.add(c);
+				finishedCargoString += (" "+c.getCargo());
 			}
+			
+			//TODO Length checking on this string so that lines wrap properly
+			textCanvas.clear();
+			textCanvas.drawText(finishedCargoString, 0, 0);
+			log().debug("Finished: " + finishedCargoString);
+			
+			/*once the train has moved completely into the end component and we have extracted it's cargo
+			 * it doesn't need to be visually represented.
+			 */
+			train.getLayer().destroy();
 		}
+		
 	}
 
 	@Override
 	public void takeTrain(UITrain train) {
 		mIncomming.add(train);
-		log().debug("Got a train to clone.");
+		log().debug("Reached end of line. All change please.");
 	}
 
 	@Override
