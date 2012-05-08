bba7ba20907f82ba9b001fca64f022db8305ff03
Thomas Dybdahl Ahle <thomas@ahle.dk>
2012-05-08 23:22:44 +0100

    Fixed pause not working for black boxes

--- core/src/main/java/com/github/thomasahle/trainbox/trainbox/uimodel/BlackBoxComponent.java
+++ core/src/main/java/com/github/thomasahle/trainbox/trainbox/uimodel/BlackBoxComponent.java
@@ -38,6 +38,9 @@ public abstract class BlackBoxComponent extends AbstractComponent {
 	public void update(float delta) {
 		// TODO: We should indicate if the component is loaded or not.
 		
+		if (paused())
+			return;
+		
 		float compLeft = getDeepPosition().x;
 		float compRight = compLeft + getSize().width;
 	
