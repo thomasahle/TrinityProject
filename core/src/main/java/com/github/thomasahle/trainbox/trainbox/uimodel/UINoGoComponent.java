e4185de770a0dcf36737179504f47480b6d7f210
Thomas Dybdahl Ahle <thomas@ahle.dk>
2012-05-14 01:19:45 +0100

    Youll love this one

---
+++ core/src/main/java/com/github/thomasahle/trainbox/trainbox/uimodel/UINoGoComponent.java
@@ -0,0 +1,68 @@
+package com.github.thomasahle.trainbox.trainbox.uimodel;
+
+import static playn.core.PlayN.graphics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.github.thomasahle.trainbox.trainbox.util.CanvasHelper;
+
+import playn.core.Canvas;
+import playn.core.CanvasImage;
+import playn.core.Layer;
+import playn.core.Path;
+import pythagoras.f.Dimension;
+
+public class UINoGoComponent extends AbstractComponent implements UIComponent, TrainTaker {
+
+	private final static int HEIGHT = 100;
+	private int mWidth;
+	
+	private Layer mBackLayer, mFrontLayer;
+	
+	@Override
+	public List<UITrain> getTrains() {
+	    return Collections.emptyList();
+	}
+	
+	public UINoGoComponent(int width) {
+		mWidth = width;
+		
+		mFrontLayer = CanvasHelper.newEmptyLayer();
+		
+		CanvasImage image = graphics().createImage(width, HEIGHT);
+		image.canvas().setFillColor(0xff000000).fillRect(0, 0, width, HEIGHT);
+		mBackLayer = graphics().createImageLayer(image);
+	}
+	
+	@Override
+	public Dimension getSize() {
+		return new Dimension(mWidth, HEIGHT);
+	}
+
+	@Override
+	public Layer getBackLayer() {
+		return mBackLayer;
+	}
+
+	@Override
+	public Layer getFrontLayer() {
+		return mFrontLayer;
+	}
+
+	@Override
+	public void update(float delta) {
+	}	
+	
+	@Override
+	public void takeTrain(UITrain train) {
+		throw new UnsupportedOperationException("Hey! It's 'No Go'!");
+	}
+
+	@Override
+	public float leftBlock() {
+		return getDeepPosition().x;
+	}
+}
