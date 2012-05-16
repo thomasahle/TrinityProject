thomasahle/TrinityProject

commit 616ed64202bd952925212ba4771356f774872a14

Android works!! Replaced mouse listeners with pointer listeners.

core/src/main/java/com/github/thomasahle/trainbox/trainbox/scenes/LevelScene.java
@@ -1,6 +1,5 @@
 package com.github.thomasahle.trainbox.trainbox.scenes;
 
-import static playn.core.PlayN.mouse;
 import static playn.core.PlayN.assets;
 import static playn.core.PlayN.graphics;
 import static playn.core.PlayN.keyboard;
@@ -19,10 +18,6 @@ import playn.core.Keyboard;
 import playn.core.TextFormat;
 import playn.core.Keyboard.TypedEvent;
 import playn.core.Layer;
-import playn.core.Mouse;
-import playn.core.Mouse.ButtonEvent;
-import playn.core.Mouse.MotionEvent;
-import playn.core.Mouse.WheelEvent;
 import playn.core.Pointer;
 import playn.core.Pointer.Event;
 import playn.core.TextFormat.Alignment;
@@ -44,7 +39,7 @@ import com.github.thomasahle.trainbox.trainbox.uimodel.UILevel;
  *  - Components to add
  *  - The play button
  */
-public class LevelScene implements Scene, Mouse.Listener, Pointer.Listener, Keyboard.Listener {
+public class LevelScene implements Scene, Pointer.Listener, Keyboard.Listener {
 	private static final float SPEED_INCREASE = 0.005f;
 	private static final int MENU_HEIGHT = 200;
 	private final static int HEIGHT = graphics().screenHeight();
@@ -107,7 +102,7 @@ public class LevelScene implements Scene, Mouse.Listener, Pointer.Listener, Keyb
 		graphics().rootLayer().add(levelStatusLayer);
 		graphics().rootLayer().add(levelPopupLayer);
 		keyboard().setListener(this);
-		mouse().setListener(this);
+		pointer().setListener(this);
 	}
 
 	@Override
@@ -121,7 +116,7 @@ public class LevelScene implements Scene, Mouse.Listener, Pointer.Listener, Keyb
 		graphics().rootLayer().remove(levelStatusLayer);
 		graphics().rootLayer().remove(levelPopupLayer);
 		keyboard().setListener(null);
-		mouse().setListener(null);
+		pointer().setListener(null);
 	}
 	
 	private void setPaused(boolean paused) {
@@ -455,15 +450,15 @@ public class LevelScene implements Scene, Mouse.Listener, Pointer.Listener, Keyb
 	@Override public void onKeyTyped(TypedEvent event) {}
 	@Override public void onKeyUp(playn.core.Keyboard.Event event) {}
 
-	@Override public void onMouseDown(ButtonEvent event) {
-		// Unfortunately this doesn't quite work.
-		// The pointer listener will still register a click. Whatever.
-		if (event.button() != Mouse.BUTTON_LEFT) {
-			toolMan.unselect();
-			event.setPreventDefault(true);
-		}
-	}
-	@Override public void onMouseUp(ButtonEvent event) {}
-	@Override public void onMouseMove(MotionEvent event) {}
-	@Override public void onMouseWheelScroll(WheelEvent event) {}
+//	@Override public void onMouseDown(ButtonEvent event) {
+//		// Unfortunately this doesn't quite work.
+//		// The pointer listener will still register a click. Whatever.
+//		if (event.button() != Mouse.BUTTON_LEFT) {
+//			toolMan.unselect();
+//			event.setPreventDefault(true);
+//		}
+//	}
+//	@Override public void onMouseUp(ButtonEvent event) {}
+//	@Override public void onMouseMove(MotionEvent event) {}
+//	@Override public void onMouseWheelScroll(WheelEvent event) {}
 }

core/src/main/java/com/github/thomasahle/trainbox/trainbox/scenes/DemoScene.java
@@ -2,33 +2,18 @@ package com.github.thomasahle.trainbox.trainbox.scenes;
 
 import static playn.core.PlayN.assets;
 import static playn.core.PlayN.graphics;
-import static playn.core.PlayN.keyboard;
 import static playn.core.PlayN.pointer;
 
-import java.awt.Button;
-import java.awt.Component;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-import javax.swing.plaf.metal.MetalButtonUI;
 
 import playn.core.Canvas;
 import playn.core.CanvasImage;
-import playn.core.Events;
 import playn.core.GroupLayer;
 import playn.core.Image;
 import playn.core.ImageLayer;
-import playn.core.Key;
-import playn.core.Keyboard;
-import playn.core.Keyboard.Event;
-import playn.core.Keyboard.TypedEvent;
-import playn.core.Mouse;
-import playn.core.Mouse.ButtonEvent;
-import playn.core.Mouse.MotionEvent;
-import playn.core.Mouse.WheelEvent;
 import playn.core.Pointer;
-import playn.core.Pointer.Listener;
-import playn.core.SurfaceLayer;
+import playn.core.Pointer.Event;
 
 import com.github.thomasahle.trainbox.trainbox.core.TrainBox;
 
@@ -118,92 +103,80 @@ public class DemoScene implements Scene, Pointer.Listener {
         final Image backButtonImage = assets().getImage("images/pngs/backButton.png");
         backButtonImageLayer = graphics().createImageLayer(backButtonImage);
         demoLayer.add(backButtonImageLayer);
-        backButtonImageLayer.setTranslation(20, 520);
-        backButtonImageLayer.setVisible(false);
-        backButtonImageLayer.addListener(new Mouse.Listener() {
+		backButtonImageLayer.setTranslation(20, 520);
+		backButtonImageLayer.setVisible(false);
+		backButtonImageLayer.addListener(new Pointer.Listener() {
 
 			@Override
-			public void onMouseDown(ButtonEvent event) {
+			public void onPointerStart(Pointer.Event event) {
 				demoPages.get(currentDemoIndex).setVisible(false);
-				demoPages.get(currentDemoIndex-1).setVisible(true);
+				demoPages.get(currentDemoIndex - 1).setVisible(true);
 				nextButtonImageLayer.setVisible(true);
 				doneButtonImageLayer.setVisible(false);
-				if ((currentDemoIndex-1) == 0) 
+				if ((currentDemoIndex - 1) == 0)
 					backButtonImageLayer.setVisible(false);
-				currentDemoIndex= currentDemoIndex-1;
+				currentDemoIndex = currentDemoIndex - 1;
 			}
 
 			@Override
-			public void onMouseUp(ButtonEvent event) {
+			public void onPointerEnd(Event event) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
-			public void onMouseMove(MotionEvent event) {
+			public void onPointerDrag(Event event) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
-			@Override
-			public void onMouseWheelScroll(WheelEvent event) {
-				// TODO Auto-generated method stub
-				
-			}});
+		});
         
         
         final Image nextButtonImage = assets().getImage("images/pngs/nextButton.png");
         nextButtonImageLayer = graphics().createImageLayer(nextButtonImage);
         demoLayer.add(nextButtonImageLayer);
-        nextButtonImageLayer.setTranslation(680, 520);
-        nextButtonImageLayer.addListener(new Mouse.Listener() {
+		nextButtonImageLayer.setTranslation(680, 520);
+		nextButtonImageLayer.addListener(new Pointer.Listener() {
 
 			@Override
-			public void onMouseDown(ButtonEvent event) {
+			public void onPointerStart(Pointer.Event event) {
 				demoPages.get(currentDemoIndex).setVisible(false);
-				demoPages.get(currentDemoIndex+1).setVisible(true);
+				demoPages.get(currentDemoIndex + 1).setVisible(true);
 				backButtonImageLayer.setVisible(true);
-				if ((currentDemoIndex+1) == demoPages.size()-1) {
+				if ((currentDemoIndex + 1) == demoPages.size() - 1) {
 					nextButtonImageLayer.setVisible(false);
 					doneButtonImageLayer.setVisible(true);
-				}
-				else {
+				} else {
 					doneButtonImageLayer.setVisible(false);
 				}
 
-				currentDemoIndex= currentDemoIndex+1;
+				currentDemoIndex = currentDemoIndex + 1;
 
 			}
 
 			@Override
-			public void onMouseUp(ButtonEvent event) {
+			public void onPointerEnd(Event event) {
 				// TODO Auto-generated method stub
 				
 			}
 
 			@Override
-			public void onMouseMove(MotionEvent event) {
+			public void onPointerDrag(Event event) {
 				// TODO Auto-generated method stub
 				
 			}
+		});
 
-			@Override
-			public void onMouseWheelScroll(WheelEvent event) {
-				// TODO Auto-generated method stub
-				
-			}});
-        
-        
-
-        final Image doneButtonImage = assets().getImage("images/pngs/doneButton.png");
+		final Image doneButtonImage = assets().getImage("images/pngs/doneButton.png");
         doneButtonImageLayer = graphics().createImageLayer(doneButtonImage);
         demoLayer.add(doneButtonImageLayer);
-        doneButtonImageLayer.setTranslation(680, 520);
-        doneButtonImageLayer.setVisible(false);
-        doneButtonImageLayer.addListener(new Mouse.Listener() {
+		doneButtonImageLayer.setTranslation(680, 520);
+		doneButtonImageLayer.setVisible(false);
+		doneButtonImageLayer.addListener(new Pointer.Listener() {
 
 			@Override
-			public void onMouseDown(ButtonEvent event) {
+			public void onPointerStart(Pointer.Event event) {
 				demoPages.get(currentDemoIndex).setVisible(false);
 				// initialize for the next run of the demo
 				demoPages.get(0).setVisible(true);
@@ -212,32 +185,25 @@ public class DemoScene implements Scene, Pointer.Listener {
 				backButtonImageLayer.setVisible(false);
 				nextButtonImageLayer.setVisible(true);
 
-
 				trainBox.setScene(trainBox.getStartScene());
 
-
 			}
 
 			@Override
-			public void onMouseUp(ButtonEvent event) {
+			public void onPointerEnd(Event event) {
 				// TODO Auto-generated method stub
 				
 			}
 
 			@Override
-			public void onMouseMove(MotionEvent event) {
+			public void onPointerDrag(Event event) {
 				// TODO Auto-generated method stub
 				
 			}
-
-			@Override
-			public void onMouseWheelScroll(WheelEvent event) {
-				// TODO Auto-generated method stub
-				
-			}});
+		});
 
 	}
-	
+
 	@Override
 	public void onAttach() {
 		graphics().rootLayer().add(bgLayer);

core/src/main/java/com/github/thomasahle/trainbox/trainbox/scenes/StartScene.java
@@ -17,10 +17,6 @@ import playn.core.Key;
 import playn.core.Keyboard;
 import playn.core.Keyboard.Event;
 import playn.core.Keyboard.TypedEvent;
-import playn.core.Mouse;
-import playn.core.Mouse.ButtonEvent;
-import playn.core.Mouse.MotionEvent;
-import playn.core.Mouse.WheelEvent;
 import playn.core.Pointer;
 
 import com.github.thomasahle.trainbox.trainbox.core.TrainBox;
@@ -79,34 +75,25 @@ public class StartScene implements Scene, Keyboard.Listener, Pointer.Listener {
         final ImageLayer aboutBlurBackButtonImageLayer = graphics().createImageLayer(aboutBlurBackButtonImage);
         aboutLayer.add(aboutBlurBackButtonImageLayer);
         aboutBlurBackButtonImageLayer.setTranslation(680, 520);
-        aboutBlurBackButtonImageLayer.addListener(new Mouse.Listener() {
-
+        aboutBlurBackButtonImageLayer.addListener(new Pointer.Listener() {
 			@Override
-			public void onMouseDown(ButtonEvent event) {
+			public void onPointerStart(playn.core.Pointer.Event event) {
 				aboutLayer.setVisible(false);
-				
 			}
 
 			@Override
-			public void onMouseUp(ButtonEvent event) {
+			public void onPointerEnd(playn.core.Pointer.Event event) {
 				// TODO Auto-generated method stub
 				
 			}
 
 			@Override
-			public void onMouseMove(MotionEvent event) {
+			public void onPointerDrag(playn.core.Pointer.Event event) {
 				// TODO Auto-generated method stub
 				
 			}
-
-			@Override
-			public void onMouseWheelScroll(WheelEvent event) {
-				// TODO Auto-generated method stub
-				
-			}});
-        
-        
-        
+		});
+        		
         menuLayer = graphics().createGroupLayer();
         menuLayer.setTranslation(width/5, height/4+40);
         final Image menuBackgoundImage = assets().getImage("images/pngs/menuBackground.png");
@@ -117,103 +104,77 @@ public class StartScene implements Scene, Keyboard.Listener, Pointer.Listener {
         final ImageLayer aboutButtonImageLayer = graphics().createImageLayer(aboutButtonImage);
         menuLayer.add(aboutButtonImageLayer);
         aboutButtonImageLayer.setTranslation(205,240);
-        aboutButtonImageLayer.addListener(new Mouse.Listener() {
-
+        aboutButtonImageLayer.addListener(new Pointer.Listener() {
 			@Override
-			public void onMouseDown(ButtonEvent event) {
+			public void onPointerStart(playn.core.Pointer.Event event) {
 		        aboutLayer.setVisible(true);
-				
 			}
 
 			@Override
-			public void onMouseUp(ButtonEvent event) {
+			public void onPointerEnd(playn.core.Pointer.Event event) {
 				// TODO Auto-generated method stub
 				
 			}
 
 			@Override
-			public void onMouseMove(MotionEvent event) {
+			public void onPointerDrag(playn.core.Pointer.Event event) {
 				// TODO Auto-generated method stub
 				
 			}
-
-			@Override
-			public void onMouseWheelScroll(WheelEvent event) {
-				// TODO Auto-generated method stub
-				
-			}}
-        );
-        
-        
-
+		});
         		
-       
         final Image demoButtonImage = assets().getImage("images/pngs/demoButton.png");
         final ImageLayer demoButtonImageLayer = graphics().createImageLayer(demoButtonImage);
         menuLayer.add(demoButtonImageLayer);
         demoButtonImageLayer.setTranslation(110,80);
-        demoButtonImageLayer.addListener(new Mouse.Listener() {
-           Image demoButtonPressedImage = assets().getImage("images/pngs/demoButtonPressed.png");
-            
-			@Override
-			public void onMouseWheelScroll(WheelEvent event) {	
-			}
+        demoButtonImageLayer.addListener(new Pointer.Listener() {
+        	
+          Image demoButtonPressedImage = assets().getImage("images/pngs/demoButtonPressed.png");
 
-			
 			@Override
-			public void onMouseUp(ButtonEvent event) {
-		        demoButtonImageLayer.setImage(demoButtonPressedImage);
-
-				
-			}
-			
-			@Override
-			public void onMouseMove(MotionEvent event) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onMouseDown(ButtonEvent event) {
+			public void onPointerStart(playn.core.Pointer.Event event) {
 		        demoButtonImageLayer.setImage(demoButtonPressedImage);
 				//trainBox.setScene(trainBox.getDemoScene());
 			}
-		});
-        
-        
+
+			@Override
+			public void onPointerEnd(playn.core.Pointer.Event event) {
+		        demoButtonImageLayer.setImage(demoButtonPressedImage);
+				
+			}
+
+			@Override
+			public void onPointerDrag(playn.core.Pointer.Event event) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        });
+        		        
         final Image playButtonImage = assets().getImage("images/pngs/playButton.png");
         final ImageLayer playButtonImageLayer = graphics().createImageLayer(playButtonImage);
         menuLayer.add(playButtonImageLayer);
         playButtonImageLayer.setTranslation(280,50);
-        playButtonImageLayer.addListener(new Mouse.Listener() {
-            Image playButtonPressedImage = assets().getImage("images/pngs/playButtonPressed.png");
-            
-			@Override
-			public void onMouseWheelScroll(WheelEvent event) {	
-			}
+        playButtonImageLayer.addListener(new Pointer.Listener() {
+        	
+        	Image playButtonPressedImage = assets().getImage("images/pngs/playButtonPressed.png");
 
-			
 			@Override
-			public void onMouseUp(ButtonEvent event) {
-		        playButtonImageLayer.setImage(playButtonPressedImage);
-
-				
-			}
-			
-			@Override
-			public void onMouseMove(MotionEvent event) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onMouseDown(ButtonEvent event) {
+			public void onPointerStart(playn.core.Pointer.Event event) {
 		        playButtonImageLayer.setImage(playButtonPressedImage);
 				trainBox.setScene(trainBox.getLevelSelectScene());
 			}
-		});
-      
-        
+
+			@Override
+			public void onPointerEnd(playn.core.Pointer.Event event) {
+		        playButtonImageLayer.setImage(playButtonPressedImage);
+			}
+
+			@Override
+			public void onPointerDrag(playn.core.Pointer.Event event) {
+			}
+        	
+        });
         
         bgLayer = graphics().createImageLayer(bgImage);
 	    cloudLayer.setTranslation(a, b);

test
@@ -1,0 +1,2 @@
+
+iHello 

commit caa6672a782f54689011462dc4aece6852578321

Fixed scroll bug regression from 616ed642

core/src/main/java/com/github/thomasahle/trainbox/trainbox/scenes/LevelScene.java
@@ -103,7 +103,6 @@ public class LevelScene implements Scene, Pointer.Listener, Keyboard.Listener {
 		graphics().rootLayer().add(levelStatusLayer);
 		graphics().rootLayer().add(levelPopupLayer);
 		keyboard().setListener(this);
-		pointer().setListener(this);
 	}
 
 	@Override
@@ -117,7 +116,6 @@ public class LevelScene implements Scene, Pointer.Listener, Keyboard.Listener {
 		graphics().rootLayer().remove(levelStatusLayer);
 		graphics().rootLayer().remove(levelPopupLayer);
 		keyboard().setListener(null);
-		pointer().setListener(null);
 	}
 	
 	private void setPaused(boolean paused) {
@@ -464,16 +462,4 @@ public class LevelScene implements Scene, Pointer.Listener, Keyboard.Listener {
 	}
 	@Override public void onKeyTyped(TypedEvent event) {}
 	@Override public void onKeyUp(playn.core.Keyboard.Event event) {}
-
-//	@Override public void onMouseDown(ButtonEvent event) {
-//		// Unfortunately this doesn't quite work.
-//		// The pointer listener will still register a click. Whatever.
-//		if (event.button() != Mouse.BUTTON_LEFT) {
-//			toolMan.unselect();
-//			event.setPreventDefault(true);
-//		}
-//	}
-//	@Override public void onMouseUp(ButtonEvent event) {}
-//	@Override public void onMouseMove(MotionEvent event) {}
-//	@Override public void onMouseWheelScroll(WheelEvent event) {}
 }